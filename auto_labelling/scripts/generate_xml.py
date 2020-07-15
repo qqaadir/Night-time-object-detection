858225ee68e02103ee1897a243f85ac6fa67d66b
Ali Qadar <qqaadir@yahoo.com>
2020-07-15 16:04:22 +1000

    extended auto labelling method for object detection to handle more than one classes

---
+++ auto_labelling/scripts/generate_xml.py
@@ -0,0 +1,40 @@
+import xml.etree.cElementTree as ET
+from random import randrange
+import os
+
+class GenerateXml(object):
+    def __init__(self, box_array, im_width, im_height, inferred_class, file_name):
+        self.inferred_class = inferred_class
+        self.box_array = box_array
+        self.im_width = im_width
+        self.im_height = im_height
+        self.file_name = file_name
+        print(file_name)
+
+    def gerenate_basic_structure(self):
+        annotation = ET.Element("annotation")
+        ET.SubElement(annotation, "filename").text = self.file_name + ".jpg"
+        size = ET.SubElement(annotation, "size")
+        ET.SubElement(size, "width").text = str(self.im_width)
+        ET.SubElement(size, "height").text = str(self.im_height)
+        ET.SubElement(size, "depth").text = "3"
+        
+        for j,i in enumerate(self.box_array):
+            objectBox = ET.SubElement(annotation, "object")
+            ET.SubElement(objectBox, "name").text = self.inferred_class[j]
+            ET.SubElement(objectBox, "pose").text = "Unspecified"
+            ET.SubElement(objectBox, "truncated").text = "0"
+            ET.SubElement(objectBox, "difficult").text = "0"
+            bndBox = ET.SubElement(objectBox, "bndbox")
+            ET.SubElement(bndBox, "xmin").text = str(round(i['xmin']))
+            ET.SubElement(bndBox, "ymin").text = str(round(i['ymin']))
+            ET.SubElement(bndBox, "xmax").text = str(round(i['xmax']))
+            ET.SubElement(bndBox, "ymax").text = str(round(i['ymax']))
+       
+
+        arquivo = ET.ElementTree(annotation)
+        arquivo.write(r"add_your_path_here/" + self.file_name + ".xml")
+
+def main():
+    xml = GenerateXml([{'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}], '4000', '2000', 'miner') # just for debuggind
+    xml.gerenate_basic_structure()    
